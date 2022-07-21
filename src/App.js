4b280bb701c94580b9f60348e7fa85ca4af418b9
amirxoder <amirxoder@gmail.com>
2022-07-21 17:50:02 +0430

    starter styling and routes complited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
 
-function App() {
+//syles
+import "./App.css";
+
+
+//components
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/" element={<Homepage />} />
+              <Route path="/*" element={<Navigate to="/" />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
     </div>
   );
-}
+};
 
 export default App;
